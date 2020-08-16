d87ba9ca7e577cfce52eed8d4e963c8f5326cbd7
lzqok <lzqok@lzqok.com>
2020-08-16 17:38:10 +0800

    Initial Commit

---
+++ EmptyRouter/EmptyRouter.h
@@ -0,0 +1,19 @@
+//
+//  EmptyRouter.h
+//  EmptyRouter
+//
+//  Created by empty on 2020/8/16.
+//  Copyright © 2020 empty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmptyRouter.
+FOUNDATION_EXPORT double EmptyRouterVersionNumber;
+
+//! Project version string for EmptyRouter.
+FOUNDATION_EXPORT const unsigned char EmptyRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmptyRouter/PublicHeader.h>
+
+
